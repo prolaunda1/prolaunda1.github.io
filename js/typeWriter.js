81ab374f55df803870c22738234213305892caac
prolaunda1 <93452114+prolaunda1@users.noreply.github.com>
2021-10-30 14:40:32 -0700

    Update typeWriter.js

--- js/typeWriter.js
+++ js/typeWriter.js
@@ -1,7 +1,7 @@
 
 let i = 0;
-let text1 = "Hey! Miss Qiu.";
-let text2 = "Today is the Big Day."
+let text1 = "Hey! Miss Trishla.";
+let text2 = "Love You So Much."
 let speed = 100;
 
 function typeWriter(text, para){
